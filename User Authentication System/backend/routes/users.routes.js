7e106c26d7ffbe295d7c426399d6e49cdc8fcc1a
Satyajeet Kumar Rao <satyajeetrao.rao2@gmail.com>
2024-06-08 14:18:50 +0530

    logout functionality added

--- User Authentication System/backend/routes/users.routes.js
+++ User Authentication System/backend/routes/users.routes.js
@@ -61,6 +61,7 @@ usersRouter.post("/login", validateLogin, async (req, res) => {
                 httpOnly: true,
                 expires: new Date(Date.now() + 24 * 60 * 60 * 1000),
                 secure: process.env.NODE_ENV === "production",
+                sameSite: "none",
               })
               .json({
                 accessToken,
